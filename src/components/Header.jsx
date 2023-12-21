200d2d24017086077e98e68de6375585d853a0e3
Boy753hub <boy753123@gmail.com>
2023-12-21 17:10:57 +0400

    added modal and login styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Header.module.css'
 import logo from '../assets/LOGO.png'
+import Modal from './Modal'
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+  const [login, setLogin] = useState('')
+
+  const openModal = () => {
+    setOpen(last => last = !last)
+    setLogin('login')
+  }
+
   return (
     <div className={styles.Header}>
       <div className={styles.HeaderContent}>
         <div className={styles.logo}>
           <img src={logo} alt="here was logo" />
         </div>
-        <div className={styles.login}>
+        <div className={styles.login} onClick={openModal}>
             შესვლა
         </div>
       </div>
+        {open ? <Modal value={login} setOpen={setOpen} /> : " "}
     </div>
   )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../styles/Modal.module.css'
+import x from '../assets/X.png'
+import info_circle from '../assets/info-circle.png'
+const Modal = ({ value, setOpen }) => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
 
-const Modal = () => {
+  const closeModal = () => {
+    setOpen((last) => !last);
+  };
+
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
+  const onLogin = (e) => {
+    e.preventDefault();
+
+    if (email.endsWith('@redberry.ge')) {
+      // Perform login logic
+      console.log('Login successful');
+    } else {
+      setError('მეილი უნდა მთავრდებოდეს @redberry.ge–ით');
+    }
+  };
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    setError(''); // Clear the error when the user types
+  };
+
   return (
-    <div>Modal</div>
-  )
-}
+    <div className={styles.modalBackground} onClick={closeModal}>
+      <div className={styles.modalContent} onClick={stopPropagation}>
+        <img src={x} alt="x" className={styles.x} onClick={closeModal} />
+        {value === 'login' ? (
+          <div className={styles.login}>
+            <h1>შესვლა</h1>
+            <p>ელ–ფოსტა</p>
+            <form onSubmit={onLogin}>
+              <input
+                type="email"
+                placeholder="Example@redberry.ge"
+                value={email}
+                onChange={handleChange}
+                className={error ? styles.errorInput : ''}
+              />
+              {error && <div className={styles.info}> <img src={info_circle} alt="!"  /><p className={styles.error}> {error}</p></div>}
+              <button type="submit">შესვლა</button>
+            </form>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
